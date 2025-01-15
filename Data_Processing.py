340afea4d21fba7bc59c62068a70579132342b92
miguelvella12 <miguelvella12@gmail.com>
2025-01-15 14:55:50 +0100

    Adding data function to restructure Sentinel-5P data

--- Data_Processing.py
+++ Data_Processing.py
@@ -81,13 +81,55 @@ def convert_geotiff_to_csv(directory, output_csv):
 
     return
 
+def change_sentinel5_csv_structure(s5_file_path, s2_file_path, output_csv_path):
+    """
+    Change the structure of Sentinel-5P csv to match Sentinel-2 (due to uniform CO values per year)
+
+    :param s5_file_path: Path to the Sentinel-5P csv file
+    :param s2_file_path: Path to the Sentinel-2 csv file
+    :param output_csv_path: Path to the output CSV file
+    :return:
+    """
+
+    s5_data = pd.read_csv(s5_file_path)
+    s2_data = pd.read_csv(s2_file_path)
+
+    # Create a mapping for Sentinel-5P values for each date
+    s5_date_mapping = s5_data.set_index("Date")['Value'].to_dict()
+
+    # Replicate Sentinel-5P data to match Sentinel-2 structure
+    new_s5_date_mapping = [
+        {
+            'Date': s2_row['Date'],
+            'Coordinates': s2_row['Coordinates'],
+            'Value': s5_date_mapping.get(s2_row['Date'], None)
+        }
+        for _, s2_row in s2_data.iterrows()
+    ]
+
+    # Create new DataFrame for new Sentinel-5P data
+    new_s5_df = pd.DataFrame(new_s5_date_mapping)
+
+    # Save
+    new_s5_df.to_csv(output_csv_path, index=False)
+    print(f"CSV file written to {output_csv_path}")
+
+    return
 
 # Update the `directory` path to the folder containing your files
-directory = "Datasets/Sentinel-2/Sentinel2_Vondel"
+# directory = "Datasets/Sentinel-2/Sentinel2_Vondel"
 # old_prefix = "openEO"
 # new_prefix = "NO2_Vondel"
 # rename_files(directory, old_prefix, new_prefix)
 
 # Convert GeoTIFF files to .csv file
-output_csv = "Datasets/Sentinel-2/Sentinel2_Vondel/Sentinel2_Vondel_CSV.csv"
-convert_geotiff_to_csv(directory, output_csv)
+# output_csv = "Datasets/Sentinel-2/Sentinel2_Vondel/Sentinel2_Vondel_CSV.csv"
+# convert_geotiff_to_csv(directory, output_csv)
+
+change_sentinel5_csv_structure(s5_file_path="Datasets/Sentinel-5P/CO_Vondel/CO_Vondel_CSV.csv",
+                               s2_file_path="Datasets/Sentinel-2/Sentinel2_Vondel/Sentinel2_Vondel_CSV.csv",
+                               output_csv_path="Datasets/Sentinel-5P/CO_Vondel/CO_Vondel_CSV.csv")
+
+change_sentinel5_csv_structure(s5_file_path="Datasets/Sentinel-5P/NO2_Vondel/NO2_Vondel_CSV.csv",
+                               s2_file_path="Datasets/Sentinel-2/Sentinel2_Vondel/Sentinel2_Vondel_CSV.csv",
+                               output_csv_path="Datasets/Sentinel-5P/NO2_Vondel/NO2_Vondel_CSV.csv")
